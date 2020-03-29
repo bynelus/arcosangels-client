02cb918ed64066889eb816e232debaff66a88705
Niels Koole <niels@getroadmap.com>
2020-03-29 11:02:21 +0200

    Ride details added

--- Sources/App/JSON Models/DetailedUserModel.swift
+++ Sources/App/JSON Models/DetailedUserModel.swift
@@ -2,9 +2,12 @@ import Vapor
 
 struct DetailedUserModel: Content {
 	let name: String
-	let hasData: Bool
 	let totalRuns: String
 	let averageRunDistance: String
 	let averageRunMinutePerKM: String
 	let totalRunDistance: String
+	let totalRides: String
+	let averageRideDistance: String
+	let averageRideSpeed: String
+	let totalRideDistance: String
 }
--- Sources/App/Mappers/DetailedUserModelMapper.swift
+++ Sources/App/Mappers/DetailedUserModelMapper.swift
@@ -3,16 +3,25 @@ import Vapor
 final class DetailedUserModelMapper {
 	static func map(_ model: UserSummary) -> DetailedUserModel {
 		let name = mapName(firstName: model.firstName, lastName: model.lastName)
-		
-		guard model.runTotalActivities > 0 else {
-			return DetailedUserModel(name: name, hasData: false, totalRuns: "0", averageRunDistance: "0", averageRunMinutePerKM: "00:00", totalRunDistance: "0")
-		}
-		
 		let totalRuns = "\(model.runTotalActivities)"
 		let averageRunDistance = (model.runAverageDistance / 1000).round(with: 2).replacingOccurrences(of: ".", with: ",")
 		let averageRunMinutePerKM = mapAverageMinutePerKM(averageMovingTime: model.runAverageMovingTime, averageDistance: model.runAverageDistance)
-		let totalDistance = (model.runTotalDistance / 1000).round(with: 2).replacingOccurrences(of: ".", with: ",")
-		return DetailedUserModel(name: name, hasData: true, totalRuns: totalRuns, averageRunDistance: averageRunDistance, averageRunMinutePerKM: averageRunMinutePerKM, totalRunDistance: totalDistance)
+		let totalRunDistance = (model.runTotalDistance / 1000).round(with: 2).replacingOccurrences(of: ".", with: ",")
+		
+		let totalRides = "\(model.rideTotalActivities)"
+		let averageRideDistance = (model.rideAverageDistance / 1000).round(with: 2).replacingOccurrences(of: ".", with: ",")
+		let averageRideSpeed = mapAverageKMPerHour(averageMovingTime: model.rideAverageMovingTime, averageDistance: model.rideAverageDistance)
+		let totalRideDistance = (model.rideTotalDistance / 1000).round(with: 2).replacingOccurrences(of: ".", with: ",")
+		
+		return DetailedUserModel(name: name,
+								 totalRuns: totalRuns,
+								 averageRunDistance: averageRunDistance,
+								 averageRunMinutePerKM: averageRunMinutePerKM,
+								 totalRunDistance: totalRunDistance,
+								 totalRides: totalRides,
+								 averageRideDistance: averageRideDistance,
+								 averageRideSpeed: averageRideSpeed,
+								 totalRideDistance: totalRideDistance)
 	}
 	
 	private static func mapName(firstName: String, lastName: String) -> String {
@@ -30,9 +39,16 @@ final class DetailedUserModelMapper {
 	}
 	
 	private static func mapAverageMinutePerKM(averageMovingTime: Double, averageDistance: Double) -> String {
+		guard averageMovingTime > 0 && averageDistance > 0 else { return "00:00" }
 		let secondsPerKM = averageMovingTime / (averageDistance / 1000)
 		let minutes = Int(floor(secondsPerKM / 60))
 		let seconds = Int(round(secondsPerKM.truncatingRemainder(dividingBy: 60)))
 		return String(format: "%02d", minutes) + ":" + String(format: "%02d", seconds)
+	}
+	
+	private static func mapAverageKMPerHour(averageMovingTime: Double, averageDistance: Double) -> String {
+		guard averageMovingTime > 0 && averageDistance > 0 else { return "00:00" }
+		let speed = averageDistance / averageMovingTime * 3600 / 1000
+		return speed.round(with: 2).replacingOccurrences(of: ".", with: ",")
 	}
 }
